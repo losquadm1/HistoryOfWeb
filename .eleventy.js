70d69c9e459c5059faba7a3135c24a5f805b8eec
losquadm1 <losquadm1@newpaltz.edu>
2024-11-25 19:23:27 -0500

    Initial commit

---
+++ .eleventy.js
@@ -0,0 +1,26 @@
+const { EleventyHtmlBasePlugin } = require("@11ty/eleventy");
+const urlBase = process.env.BASE_URL || "";
+
+module.exports = function (eleventyConfig) {
+  console.log(process.env.NODE_ENV);
+  let p = {};
+  if (process.env.NODE_ENV === "build") {
+    console.log("running in production");
+    eleventyConfig.addPlugin(EleventyHtmlBasePlugin);
+    // this should be set
+    p = { pathPrefix: urlBase };
+  }
+
+  eleventyConfig.addPassthroughCopy("src/assets");
+  eleventyConfig.addWatchTarget("./src/");
+
+  // Return your Object options:
+  return {
+    ...p,
+    dir: {
+      input: "src",
+      output: "docs",
+      includes: "_includes",
+    },
+  };
+};
